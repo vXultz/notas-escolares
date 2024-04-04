b6b08befb7a2c0f86be9154b0a6b719077d0027f
Arthur Ferreira Borba <ferreiraborbaarthur@gmail.com>
2024-04-03 22:51:11 -0300

    logs do aluno adicionados

--- src/main/java/com/senai/projetonotas/controller/AlunoController.java
+++ src/main/java/com/senai/projetonotas/controller/AlunoController.java
@@ -2,6 +2,7 @@ package com.senai.projetonotas.controller;
 
 import com.senai.projetonotas.entity.AlunoEntity;
 import com.senai.projetonotas.service.AlunoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,8 +11,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @RestController
-@RequestMapping("/aluno")
+@RequestMapping("/alunos")
 public class AlunoController {
 
     @Autowired
@@ -19,34 +21,40 @@ public class AlunoController {
 
     @GetMapping
     public ResponseEntity<List<AlunoEntity>> listarTodos(){
+        log.info("GET Alunos -> listarTodos");
         List<AlunoEntity> alunos = service.listarTodos();
+        log.info("200 OK");
         return new ResponseEntity<>(alunos, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<AlunoEntity> listarPorId(@PathVariable Long id){
-        Optional<AlunoEntity> aluno = service.listarPorId(id);
-        if (aluno.isPresent()){
-            return new ResponseEntity<>(aluno.get(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        log.info("GET Alunos -> listarPorId");
+        AlunoEntity aluno = service.listarPorId(id);
+        log.info("200 OK");
+        return new ResponseEntity<>(aluno, HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<AlunoEntity> salvar(@RequestBody AlunoEntity aluno){
+        log.info("POST Alunos -> salvar");
         AlunoEntity alunoSalvo = service.salvar(aluno);
+        log.info("201 CREATED");
         return new ResponseEntity<>(alunoSalvo, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> removerPorId(@PathVariable Long id){
+        log.info("DELETE Alunos -> removerPorId");
         service.removerPorId(id);
+        log.info("204 NO_CONTENT");
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
     @PutMapping
     public ResponseEntity<Void> atualizar(@RequestBody AlunoEntity aluno){
+        log.info("PUT Alunos -> atualizar");
         service.atualizar(aluno);
+        log.info("200 OK");
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
--- src/main/java/com/senai/projetonotas/service/AlunoService.java
+++ src/main/java/com/senai/projetonotas/service/AlunoService.java
@@ -2,33 +2,41 @@ package com.senai.projetonotas.service;
 
 import com.senai.projetonotas.entity.AlunoEntity;
 import com.senai.projetonotas.repository.AlunoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class AlunoService {
     @Autowired
     AlunoRepository repository;
 
     public List<AlunoEntity> listarTodos(){
+        log.info("todos os alunos listados");
         return repository.findAll();
     }
 
-    public Optional<AlunoEntity> listarPorId(Long id){
-        return repository.findById(id);
+    public AlunoEntity listarPorId(Long id){
+        log.info("aluno com id {} buscado", id);
+        return repository.findById(id).orElseThrow();
+
     }
 
     public AlunoEntity salvar(AlunoEntity aluno) {
+        log.info("salvando aluno com o nome {}", aluno.getNome());
         return repository.save(aluno);
     }
     public void removerPorId(Long id) {
+        log.info("removendo aluno com o id {}", id);
         repository.deleteById(id);
     }
 
     public int atualizar(AlunoEntity aluno){
+        log.info("atualizando aluno com o id {}", aluno.getId());
         return repository.update(aluno.getId(), aluno.getNome(), aluno.getDataNascimento());
     }
 }
